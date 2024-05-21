1543b15f5bb68d637d32a018013bee8944eaaa55
mvirendra1405 <mvirendra1405@gmail.com>
2024-05-21 17:45:49 +0530

    python

---
+++ 4.3 pattern.py
@@ -0,0 +1,21 @@
+row=1
+while row<=5:
+    col=1
+    while col<=5:
+        if col==1:
+            print("a",end=" ")
+        elif col==1:
+            print("b",end=" ")
+        elif row==col:
+            print("c",end=" ")
+        elif:
+            print("d",end=" ")
+        elif:
+            print("e",end=" ")
+        else:
+            print(" ",end=" ")
+        col=col+1
+    row=row+1
+    print()
+
+    
---
+++ 4HW.py
@@ -0,0 +1,31 @@
+'''i=5
+while i>=1:
+    j=1
+    while j<=i:
+        print('*',end=" ")
+        j=j+1
+    i=i-1
+    print()'''
+'''row=101
+col=5
+while row>=97:
+    print(chr(row)*col)
+    col=col-1
+    row=row-1
+    print()'''
+
+'''row=97
+i=1
+while row<=101:
+    cols = 1
+    while cols<=5-i:
+        print(" ",end=" ")
+        cols+=1
+    col=97
+    while col<=row:
+        print(chr(row),end=" ")
+        col+=1
+    i+=1
+    row+=1
+    print()''' 
+
