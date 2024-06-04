ca14531c34d003afcd235c6eab5f7f45536a3f9d
mvirendra1405 <mvirendra1405@gmail.com>
2024-06-04 22:49:14 +0530

    Python

---
+++ gase.py
@@ -0,0 +1,4 @@
+import random
+
+words = ['robert','berort','mishraji','mijirash','rajit','arijit','krushna','rushank','tanaya','yatana','shraddha',
+        'ddhashra','namita','manita','netra','terna','ashish','shisha','abhishek','shakeabi','vishal','shalvi']
