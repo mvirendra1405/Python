b6063a0f5a77b186196cbb0bf32342e786888586
mvirendra1405 <mvirendra1405@gmail.com>
2024-06-10 21:32:01 +0530

    Python

---
+++ Function.py
@@ -0,0 +1,54 @@
+#Variation in user defined function
+
+#function with no argument no return value
+#function with argument no return value
+#function with no argument but return value
+#function with argument and return value
+
+'''def add():
+    a=10
+    b=20
+    print(f'addition of (a) and (b) is:',a+b)
+add()'''
+
+'''def add(a,b):
+    print(f'addition of (a) and (b) is:',a+b)
+a=10
+b=20
+add(a,b)'''
+
+##def add( ):
+##    a=10
+##    b=20
+##    return a+b
+##    #return(f'addition of (a) and (b) is:',a+b)
+##    print('hii') #after return no executable
+##
+##sum=add()
+##print(sum)
+
+'''def add(a,b):
+    return a+b
+    #return f'addition of (a) and (b) is:',a+b
+    print('hii') #after return no executable
+
+sum=add(8,9)
+print(sum)'''
+
+def add(a,b):
+     return a+b
+     #return f'addition of (a) and (b) is:',a+b
+     print('hii') #after return no executable
+a=8
+b=9
+sum=add(a,b)
+print(sum)
+    
+    
+
+
+
+    
+
+
+    
