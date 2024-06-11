9260151808f176ed0a040d6a1daf995cee50a392
mvirendra1405 <mvirendra1405@gmail.com>
2024-06-11 22:07:22 +0530

    Python

---
+++ function.2.py
@@ -0,0 +1,21 @@
+##Types of arguement
+##1)Positional arguement 
+##2)Default argument
+##3)Keyword argument
+##4)Variable length argument
+
+'''1)Ans:During a function call,values passed through arguments should
+be in order of parameters in the function definition.'''
+
+def simple_int(p,r,t):
+    print('principle is:',p)
+    print('rate is :',r)
+    print('time is :',t)
+
+    si=p*r*t/100
+    print('simple interest is :',si)
+
+p=5000
+r=10
+t=5
+simple_int(p,r,t)
