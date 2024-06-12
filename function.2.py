f94c1409cf2f562bfbd3002338d61267001544bc
mvirendra1405 <mvirendra1405@gmail.com>
2024-06-12 21:44:08 +0530

    Python

--- function.2.py
+++ function.2.py
@@ -7,6 +7,20 @@
 '''1)Ans:During a function call,values passed through arguments should
 be in order of parameters in the function definition.'''
 
+##def simple_int(p,r,t):
+##    print('principle is:',p)
+##    print('rate is :',r)
+##    print('time is :',t)
+##
+##    si=p*r*t/100
+##    print('simple interest is :',si)
+##
+##p=5000
+##r=10
+##t=5
+##simple_int(p,r,t)
+
+
 def simple_int(p,r,t):
     print('principle is:',p)
     print('rate is :',r)
@@ -19,3 +33,18 @@ p=5000
 r=10
 t=5
 simple_int(p,r,t)
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
