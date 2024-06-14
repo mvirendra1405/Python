19e0f7e345a69d2b317b0e137e2c9c16767ce097
mvirendra1405 <mvirendra1405@gmail.com>
2024-06-14 23:07:49 +0530

    Python

---
+++ Function 4.py
@@ -0,0 +1,15 @@
+# Module and package
+# module is a file containing python definition,function and variable
+# it is a collection of function and classes
+
+# Types of module:
+# 1)built in module
+#    user defined module
+
+# syntax
+# import module_name
+# accessing a function inside module
+# module_name.fun_name()
+
+import math as m
+print(m.sqrt(4))
