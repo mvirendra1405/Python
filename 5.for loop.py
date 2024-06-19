493588dabdb00da95e5a48cdf46ed0fd0ae32dd5
mvirendra1405 <mvirendra1405@gmail.com>
2024-06-19 22:56:04 +0530

    Python

--- 5.for loop.py
+++ 5.for loop.py
@@ -7,3 +7,10 @@ for i in range(2,n,1):
         print(n,'not a prime number')
 else:
     print(n,'is an prime number')
+
+##n=37
+##for i in range(2,n,1):
+##    if n%i==0:
+##        print( n ,'not a prime number')
+##else:
+##    print( n ,'is an prime number')
