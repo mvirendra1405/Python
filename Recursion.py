dc245df2d5f75c782904363625baf0528db5cd85
mvirendra1405 <mvirendra1405@gmail.com>
2024-06-20 23:06:35 +0530

    Python

--- Recursion.py
+++ Recursion.py
@@ -16,20 +16,59 @@
 ##
 ##demo()
 
-import sys
+##import sys
+##
+##print(sys.getrecursionlimit())
+##sys.setrecursionlimit(200)
+##print(sys.getrecursionlimit())
+##
+##i=0
+##def demo():
+##    global i
+##    i=i+1
+##    print('Virendra',i)
+##    demo()
+##
+##demo()
 
-print(sys.getrecursionlimit())
-sys.setrecursionlimit(200)
-print(sys.getrecursionlimit())
+##Advantage:
+##1)clean code/less number of code
+##2)complex problem can be solved
 
-i=0
-def demo():
-    global i
-    i=i+1
-    print('Virendra',i)
-    demo()
+##Disadvantages:
+##1)hard to debug
+##2)not memory efficient
 
-demo()
+##WAP for fibbonacci series using recursion
+##0,1,1,2,3,5,......
+##
+##fibo(1)=0
+##fibo(2)=1
+##fibo(3)=fibo(1)+fibo(2)
+##fibo(3)=fibo(3-2)+fibo(3-1)
+##fibo(n)=fibo(n-2)+fibo(n-1)
+
+##def fibo(n):
+##    if n==1:
+##        return 0
+##    if n==2:
+##        return 1
+##    return fibo (n-2) + fibo(n-1)
+##n=int(input('Enter the number of terms: '))
+##print(fibo(n))
+
+
+
+
+
+
+
+
+
+
+
+
+
 
 
 
