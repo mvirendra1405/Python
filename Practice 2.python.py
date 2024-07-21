8b3696b104c3712fe54f56671f76fdb105c1533b
mvirendra1405 <mvirendra1405@gmail.com>
2024-07-21 22:46:50 +0530

    Python

--- Practice 2.python.py
+++ Practice 2.python.py
@@ -22,17 +22,27 @@
 ##else:
 ##    print("odd")
 
+##a=int(input("Enter a value: "))
+##b=int(input("Enter a value: "))
+##c=int(input("Enter a value: "))
+##
+##if(a>b and a>c):
+##    print("a is greater than both b and c")
+##elif(b>a and b>c):
+##    print("b is greater than both a and c")
+##else:
+##    print("c is greater than both a and b")
+
 a=int(input("Enter a value: "))
 b=int(input("Enter a value: "))
 c=int(input("Enter a value: "))
 
-if(a>b and a>c):
-    print("a is greater than both b and c")
-elif(b>a and b>c):
-    print("b is greater than both a and c")
+if(a<b and a<c):
+    print("a is less than both b and c")
+elif(b<a and b<c):
+    print("b is less than both a and c")
 else:
-    print("c is greater than both a and b")
-
+    print("c is less than both a and b")
 
 
 
