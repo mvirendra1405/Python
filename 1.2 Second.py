a7f34c3a599d80607b86e15fc490971169a390ea
mvirendra1405 <mvirendra1405@gmail.com>
2024-08-03 21:10:14 +0530

    Python

--- 1.2 Second.py
+++ 1.2 Second.py
@@ -1,18 +1,35 @@
-##print("hello")
-##
-##print('''hello i am virendra,
-##i live in dombivali''')
-
-x=5
+'''=5
 if x==0 :
     print("hello")
 else:
-    print("bye")
+    print("bye")'''
 
-x=int(input("Enter a number:  "))
+'''x=int(input("Enter a number:  "))
 if x>0:
     print(x,"is postive")
 elif x<0:
     print(x,"is negative")
 elif x==0:
-    print(x," is neither negative and nor positive")
+    print(x," is neither negative and nor positive")'''
+
+'''#Wap for take two number from user and find max number?
+a=int(input("Enter a number: "))
+b=int(input("Enter a number: "))
+if a>b:
+      print(a,"is greater than", b)
+elif b>a:
+    print(b,"is greater than", a)
+else:
+    print("both are equal") '''
+
+x="virendra"
+for i in x:
+    print("hello")
+
+a=[8,3,5,6,1,2,7,11,9]
+print(a[-1:-4])
+
+a=[8,3,5,6,1,2,7,11,9]
+print(a[-4:-1])
+
+
